798da01d067471889c8f1a2db36872f413e1907b
Aghmat <Aghmat@users.noreply.github.com>
2017-07-14 18:18:28 +0200

    Add files via upload
    Oops, forgot this

---
+++ LODParameters.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+//Used to determine skinned mesh renderers that have LOD
+[System.Serializable]
+public struct LODParameters
+{
+    public Transform[] bones;
+}
